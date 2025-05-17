1414199568e9e7306624cee154230cb59aa3b1de
Muhammad Baqir <mbaqir856@gmail.com>
2025-05-17 13:58:19 -0700

    Add calculator functions to buttons (equal and operators)

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
-    switch(operator) {
+    switch (operator) {
         case "+":
             return add(a, b);
         case "-":
             return subtract(a, b);
-        case "x":
+        case "X":
             return multiply(a, b);
         case "/":
             return divide(a, b);
@@ -29,17 +29,51 @@ function operate(a, b, operator) {
 
 function inputDigit() {
     const digits = document.querySelectorAll((".num-btn"));
+    const operators = document.querySelectorAll(".operator-btn");
     const text = document.querySelector(".result-text");
+    let a = "";
+    let b = "";
+    let selectedOperator = "";
     digits.forEach((digit) => {
-        digit.addEventListener(("click"), (e) => {
+        digit.addEventListener("click", (e) => {
             if (!isTextFull()) {
-                text.textContent += digit.textContent;
+                if (a && selectedOperator) {
+                    b += digit.textContent;
+                    text.textContent = b;
+                } else {
+                    a += digit.textContent;
+                    text.textContent = a;
+                }
                 console.log(text);
             }
         });
     });
-}
 
+    operators.forEach((operator) => {
+        operator.addEventListener("click", (e) => {
+            if (a && b) {
+                const result = operate(Number(a), Number(b), selectedOperator);
+                console.log(result);
+                text.textContent = result;
+                a = result;
+                b = "";
+            }
+            selectedOperator = operator.textContent;
+            console.log(operator.textContent);
+        })
+    });
+
+    const equalBtn = document.querySelector("#equal-btn");
+    equalBtn.addEventListener("click", (e) => {
+        if (a && b) {
+            const result = operate(Number(a), Number(b), selectedOperator);
+            console.log(result);
+            text.textContent = result;
+            a = result;
+            b = "";
+        }
+    });
+}
 function isTextFull() {
     const text = document.querySelector(".result-text").textContent;
     if (text.length >= 9) {
